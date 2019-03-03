8ea4c67dcd5d154436628b011d756c33c0aba8e6
yarynakorduba <korduba_y@ucu.edu.ua>
2019-03-03 16:37:25 +0200

    added React Context

--- react-enhancer/src/toggle.js
+++ react-enhancer/src/toggle.js
@@ -1,9 +1,34 @@
 import React, { Component } from "react";
 import { Switch } from "./switch";
 
+const ToggleContext = React.createContext();
+
+const ToggleConsumer = props => (
+  <ToggleContext.Consumer>
+    {context => {
+      if (!context) {
+        throw new Error(
+          "Toggle Compound Components must be rendered within the toggle component"
+        );
+      }
+      return props.children(context);
+    }}
+  </ToggleContext.Consumer>
+);
+
 class Toggle extends Component {
+  static On = ({ children }) => (
+    <ToggleConsumer>{({ on }) => (on ? children : null)}</ToggleConsumer>
+  );
+  static Off = ({ children }) => (
+    <ToggleConsumer>{({ on }) => (on ? null : children)}</ToggleConsumer>
+  );
 
-
+  static Button = props => (
+    <ToggleConsumer>
+      {({ on, toggle }) => <Switch on={on} onClick={toggle} {...props} />}
+    </ToggleConsumer>
+  );
 
   state = { on: false };
   toggle = () =>
@@ -14,12 +39,26 @@ class Toggle extends Component {
       }
     );
   render() {
-    return <Switch on={this.state.on} onClick={this.toggle} />;
+    return (
+      <ToggleContext.Provider
+        value={{ on: this.state.on, toggle: this.toggle }}
+      >
+        {this.props.children}
+      </ToggleContext.Provider>
+    );
   }
 }
 
 function Usage({ onToggle = (...args) => console.log("onToggle", ...args) }) {
-  return <Toggle onToggle={onToggle} />;
+  return (
+    <Toggle onToggle={onToggle}>
+      <div>
+        <Toggle.Button />
+      </div>
+      <Toggle.On>The button is on</Toggle.On>
+      <Toggle.Off>The button is off</Toggle.Off>
+    </Toggle>
+  );
 }
 
 export default Usage;
