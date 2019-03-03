b213d4188ce82f735335658d17044ba2370cef37
yarynakorduba <korduba_y@ucu.edu.ua>
2019-03-03 23:51:01 +0200

    add two togglers

--- react-enhancer/src/toggle.js
+++ react-enhancer/src/toggle.js
@@ -4,114 +4,45 @@ import { Switch } from "./switch";
 const callAll = (...fns) => (...args) => fns.forEach(fn => fn && fn(...args));
 
 class Toggle extends React.Component {
-  static defaultProps = {
-    onReset: () => {},
-    initialOn: false
-  };
-
-  static stateChangeTypes = {
-    reset: "__reset__",
-    toggle: "__toggle__"
-  };
-  initialState = { on: this.props.initialOn };
-  state = this.initialState;
-  internalSetState(changes, callback) {
-    this.setState(state => {
-      const changeObject =
-        typeof changes === "function" ? changes(state) : changes;
-      const reducedChanges = this.props.stateReducer(state, changeObject);
-      const { type: ignoredType, ...onlyChanges } = reducedChanges;
-      return onlyChanges;
-    }, callback);
+  state = { on: false };
+  isControlled(prop) {
+    return this.props[prop] !== undefined;
   }
-  toggle = ({ type = Toggle.stateChangeTypes.toggle } = {}) =>
-    this.internalSetState(
-      ({ on }) => ({ on: !on, type }),
-      () => {
-        this.props.onToggle && this.props.onToggle(this.state.on);
-      }
-    );
-
-  reset = () =>
-    this.internalSetState(
-      { ...this.initialState, type: Toggle.stateChangeTypes.reset },
-      () => this.props.onReset(this.state.on)
-    );
-
-  getTogglerProps = ({ onClick, className, ...props }) => {
+  getState() {
     return {
-      "aria-pressed": this.state.on,
-      onClick: callAll(onClick, () => this.toggle()),
-      className: `${className} our-custom-class-name`,
-      ...props
+      on: this.isControlled("on") ? this.props.on : this.state.on
     };
-  };
-  getStateAndHelpers = () => ({
-    on: this.state.on,
-    toggle: this.toggle,
-    getTogglerProps: this.getTogglerProps,
-    reset: this.reset
-  });
-
-  render() {
-    return this.props.children(this.getStateAndHelpers());
   }
-}
-
-function CommonToggle(props) {
-  return (
-    <Toggle {...props}>
-      {({ on, toggle }) => <Switch on={on} onClick={toggle} />}
-    </Toggle>
-  );
+  toggle = () => {
+    if (this.isControlled("on")) {
+      this.props.onToggle(!this.getState().on);
+    } else {
+      this.setState(
+        ({ on }) => ({ on: !on }),
+        () => {
+          this.props.onToggle(this.getState().on);
+        }
+      );
+    }
+  };
+  render() {
+    return <Switch on={this.props.on} onClick={this.toggle} />;
+  }
 }
 
 class Usage extends React.Component {
-  initialState = { timesClicked: 0 };
-  state = this.initialState;
-  handleToggle = (...args) => {
-    this.setState(({ timesClicked }) => ({ timesClicked: timesClicked + 1 }));
+  state = { bothOn: false };
+  handleToggle = on => {
+    this.setState({ bothOn: on });
   };
 
-  handleReset = (...args) => {
-    this.setState(this.initialState);
-  };
-
-  toggleStateReducer = (state, changes) => {
-    if (changes.type === "forced") {
-      return changes;
-    }
-    if (this.state.timesClicked >= 4) {
-      return { ...changes, on: false };
-    }
-    return changes;
-  };
   render() {
-    const { timesClicked } = this.state;
+    const { bothOn } = this.state;
     return (
-      <Toggle
-        onToggle={this.handleToggle}
-        onReset={this.handleReset}
-        stateReducer={this.toggleStateReducer}
-      >
-        {({ getTogglerProps, on, toggle, reset }) => (
-          <div>
-            <Switch on={on} {...getTogglerProps({ on })} />
-            <hr />
-            {timesClicked >= 4 && (
-              <>
-                <div>Whoa! Too much!</div>
-                <button onClick={() => toggle({ type: "forced" })}>
-                  Force toggle
-                </button>
-              </>
-            )}
-            <button onClick={reset}>Reset</button>
-
-            {on ? "on" : "off"}
-          </div>
-        )}
-      </Toggle>
+      <div>
+        <Toggle onToggle={this.handleToggle} />
+        <Toggle on={bothOn} onToggle={this.handleToggle} />
+      </div>
     );
   }
 }
