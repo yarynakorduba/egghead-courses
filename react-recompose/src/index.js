2f4fd203e454b2679f70d5cd245b8b65ef664521
yarynakorduba <korduba_y@ucu.edu.ua>
2019-03-04 20:09:43 +0200

    added lifecycle hooks

--- react-recompose/src/index.js
+++ react-recompose/src/index.js
@@ -2,107 +2,33 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import * as serviceWorker from "./serviceWorker";
-import {
-  setDisplayName,
-  setPropTypes,
-  compose,
-  withProps,
-  withHandlers,
-  withReducer
-} from "recompose";
-import PropTypes from "prop-types";
+import { lifecycle } from "recompose";
 
-const withToggle = compose(
-  withReducer("toggledOn", "dispatch", (state, action) => {
-    switch (action.type) {
-      case "SHOW":
-        return true;
-      case "HIDE":
-        return false;
-      case "TOGGLE":
-        return !state;
-      default:
-        return state;
-    }
-  }),
-  withHandlers({
-    show: ({ dispatch }) => e => dispatch({ type: "SHOW" }),
-    hide: ({ dispatch }) => e => dispatch({ type: "HIDE" }),
-    toggle: ({ dispatch }) => e => dispatch({ type: "TOGGLE" })
-  })
-);
+const config = {
+  showStatus: true,
+  canDeleteUsers: true
+};
 
-function Redux() {
-  return {
-    connect: () => BaseComponent => props => (
-      <BaseComponent
-        {...props}
-        dispatch={({ type }) => console.log(type + " dispatched")}
-      />
-    )
-  };
+function fetchConfiguration() {
+  return new Promise(resolve => {
+    setTimeout(() => resolve(config), 300);
+  });
 }
 
-const { connect } = Redux();
+const configPromise = fetchConfiguration();
 
-const StatusList = () => (
-  <div>
-    <div>pending</div>
-    <div>inactive</div>
-    <div>active</div>
-  </div>
-);
+const withConfig = lifecycle({
+  state: { config: {} },
+  componentDidMount() {
+    configPromise.then(config => this.setState({ config }));
+  }
+});
 
-const Status = withToggle(({ status, toggledOn, toggle }) => (
-  <span onClick={toggle}>
-    {status}
-    {toggledOn && <StatusList />}
-  </span>
-));
-
-const Tooltip = withToggle(({ text, children, toggledOn, show, hide }) => (
-  <div>
-    {toggledOn && (
-      <div
-        className={"Tooltip"}
-        style={{
-          background: "aliceblue",
-          color: "gray",
-          position: "fixed",
-          left: 0,
-          top: 0
-        }}
-      >
-        {text}
-      </div>
-    )}
-    <div
-      style={{ with: "30rem", height: "2rem", marginTop: "1rem" }}
-      onMouseEnter={show || (() => console.log("enter"))}
-      onMouseLeave={hide}
-    >
-      {children}
-    </div>
-  </div>
-));
-
-const enhancer = compose(
-  setDisplayName("User"),
-  setPropTypes({
-    name: PropTypes.string.isRequired,
-    status: PropTypes.string
-  }),
-  connect(),
-  withProps(({ ...props }) => console.log(props) || props)
-);
-
-const User = enhancer(({ name, status, dispatch }) => (
-  <div className={"User"} onClick={() => dispatch({ type: "USER_SELECTED" })}>
-    <Tooltip text={"Cool, Dude!"}>
-      <div>
-        {name} : <Status status={status} />
-      </div>
-    </Tooltip>
+const User = withConfig(({ name, status, config }) => (
+  <div className={"User"}>
+    {name}
+    {config && config.showStatus && "-" + status}
+    {config && config.canDeleteUsers && <button>x</button>}
   </div>
 ));
 
