454855b614f5fcf5a3bc238649bc7cee409891ba
yarynakorduba <korduba_y@ucu.edu.ua>
2019-03-04 10:13:27 +0200

    added react context

--- react-enhancer/src/toggle.js
+++ react-enhancer/src/toggle.js
@@ -1,118 +1,48 @@
-import React from "react";
+import React, { Fragment } from "react";
 import { Switch } from "./switch";
 
-const callAll = (...fns) => (...args) => fns.forEach(fn => fn && fn(...args));
+const ToggleContext = React.createContext({ on: false, toggle: () => {} });
 
 class Toggle extends React.Component {
-  static defaultProps = {
-    onToggle: () => {},
-    onStateChange: () => {}
-  };
-  static stateChangeTypes = {
-    toggleOn: "toggle-on",
-    toggleOf: "toggle-off",
-    toggle: "toggle"
-  };
-  state = { on: false };
-  isControlled(prop) {
-    return this.props[prop] !== undefined;
-  }
-  getState(state = this.state) {
-    return Object.entries(this.state).reduce((combinedState, [key, value]) => {
-      if (this.isControlled(key)) {
-        combinedState[key] = this.props[key];
-      } else {
-        combinedState[key] = value;
-      }
-      return combinedState;
-    }, {});
-  }
-
-  internalSetState(changes, callback) {
-    let allChanges;
+  static Consumer = ToggleContext.Consumer;
+  toggle = () => {
     this.setState(
-      state => {
-        const combinedState = this.getState(state);
-        const changesObject =
-          typeof changes === "function" ? changes(combinedState) : changes;
-        allChanges = changesObject;
-        const { type: ignoredType, ...onlyChanges } = changesObject;
-        const nonControlledChanges = Object.entries(onlyChanges).reduce(
-          (newChanges, [key, value]) => {
-            if (!this.isControlled(key)) {
-              newChanges[key] = value;
-            }
-            return newChanges;
-          },
-          {}
-        );
-        return Object.keys(nonControlledChanges).length
-          ? nonControlledChanges
-          : null;
-      },
+      ({ on }) => ({ on: !on }),
       () => {
-        this.props.onStateChange(allChanges, this.getState());
-        callback();
-      }
-    );
-  }
-
-  toggle = ({ on: newState, type } = {}) => {
-    this.internalSetState(
-      ({ on }) => ({
-        on: typeof newState === "boolean" ? newState : !on,
-        type
-      }),
-      () => {
-        this.props.onToggle(this.getState().on);
+        this.props.onToggle(this.state.on);
       }
     );
   };
-  handleSwitchClick = () =>
-    this.toggle({ type: Toggle.stateChangeTypes.toggle });
-  handleOffClick = () =>
-    this.toggle({ on: false, type: Toggle.stateChangeTypes.toggleOff });
-  handleOnClick = () =>
-    this.toggle({ on: true, type: Toggle.stateChangeTypes.toggleOn });
+  state = { on: false, toggle: this.toggle };
   render() {
-    return (
-      <div>
-        <Switch on={this.getState().on} onClick={this.handleSwitchClick} />
-        <button onClick={this.handleOffClick}>off</button>
-        <button onClick={this.handleOnClick}>on</button>
-      </div>
-    );
+    return <ToggleContext.Provider value={this.state} {...this.props} />;
   }
 }
 
-class Usage extends React.Component {
-  state = { bothOn: false };
-  lastWasButton = false;
-  handleStateChange = changes => {
-    const isButtonChange =
-      changes.type === Toggle.stateChangeTypes.toggleOn ||
-      changes.type === Toggle.stateChangeTypes.toggleOff;
-    if (
-      (this.lastWasButton && isButtonChange) ||
-      changes.type === Toggle.stateChangeTypes.toggle
-    ) {
-      this.setState({ bothOn: changes.on });
-      this.lastWasButton = false;
-    } else {
-      this.lastWasButton = isButtonChange;
-    }
-  };
-  render() {
-    const { bothOn } = this.state;
-    return (
-      <div>
-        <Toggle on={bothOn} onStateChange={this.handleStateChange} />
-        <Toggle on={bothOn} onStateChange={this.handleStateChange} />
-      </div>
-    );
-  }
+const Layer1 = ({ on, toggle }) => <Layer2 on={on} toggle={toggle} />;
+const Layer2 = ({ on, toggle }) => (
+  <Toggle.Consumer>
+    {({ on }) => (
+      <Fragment>
+        {on ? "The button is on" : "The button is off"}
+        <Layer3 />
+      </Fragment>
+    )}
+  </Toggle.Consumer>
+);
+const Layer3 = () => <Layer4 />;
+const Layer4 = () => (
+  <Toggle.Consumer>
+    {({ on, toggle }) => <Switch on={on} onClick={toggle} />}
+  </Toggle.Consumer>
+);
+
+function Usage({ onToggle = (...args) => console.log("onToggle", ...args) }) {
+  return (
+    <Toggle onToggle={onToggle}>
+      <Layer1 />
+    </Toggle>
+  );
 }
 
-Usage.title = "Render Props";
-
-export { Toggle, Usage as default };
+export default Usage;
