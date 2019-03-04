92c71791b31de56fbabbf5069a03e2a9fbb59c74
yarynakorduba <korduba_y@ucu.edu.ua>
2019-03-04 10:44:49 +0200

    added HOC

--- react-enhancer/src/toggle.js
+++ react-enhancer/src/toggle.js
@@ -1,7 +1,8 @@
 import React, { Fragment } from "react";
 import { Switch } from "./switch";
+import hoistNonReactStatics from "hoist-non-react-statics";
 
-const ToggleContext = React.createContext({ on: false, toggle: () => {} });
+const ToggleContext = React.createContext();
 
 class Toggle extends React.Component {
   static Consumer = ToggleContext.Consumer;
@@ -19,23 +20,32 @@ class Toggle extends React.Component {
   }
 }
 
+function withToggle(Component) {
+  const Wrapper = (props, ref) => (
+    <Toggle.Consumer>
+      {toggleContext => (
+        <Component toggle={toggleContext} {...props} ref={ref} />
+      )}
+    </Toggle.Consumer>
+  );
+  Wrapper.displayName = `withToggle(${Component.displayName ||
+    Component.name})`;
+  return hoistNonReactStatics(React.forwardRef(Wrapper), Component);
+}
+
 const Layer1 = ({ on, toggle }) => <Layer2 on={on} toggle={toggle} />;
-const Layer2 = ({ on, toggle }) => (
-  <Toggle.Consumer>
-    {({ on }) => (
-      <Fragment>
-        {on ? "The button is on" : "The button is off"}
-        <Layer3 />
-      </Fragment>
-    )}
-  </Toggle.Consumer>
-);
+const Layer2 = withToggle(function Layer2({ on }) {
+  return (
+    <Fragment>
+      {on ? "The button is on" : "The button is off"}
+      <Layer3 />
+    </Fragment>
+  );
+});
 const Layer3 = () => <Layer4 />;
-const Layer4 = () => (
-  <Toggle.Consumer>
-    {({ on, toggle }) => <Switch on={on} onClick={toggle} />}
-  </Toggle.Consumer>
-);
+const Layer4 = withToggle(({ toggle: { on, toggle } }) => (
+  <Switch on={on} onClick={toggle} />
+));
 
 function Usage({ onToggle = (...args) => console.log("onToggle", ...args) }) {
   return (
