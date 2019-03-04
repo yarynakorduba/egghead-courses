63c628aba633d5516fb11de64938a839e5d64087
yarynakorduba <korduba_y@ucu.edu.ua>
2019-03-04 19:35:38 +0200

    added onToggle Hoc

--- react-recompose/src/index.js
+++ react-recompose/src/index.js
@@ -1,25 +1,97 @@
-import React, { Component } from "react";
+import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import * as serviceWorker from "./serviceWorker";
+import {
+  setDisplayName,
+  setPropTypes,
+  compose,
+  withProps,
+  withState,
+  withHandlers
+} from "recompose";
+import PropTypes from "prop-types";
 
-const User = ({ name }) => <div className={"User"}>{name}</div>;
+const withToggle = compose(
+  withState("toggledOn", "toggle", false),
+  withHandlers({
+    show: ({ toggle }) => e => toggle(true),
+    hide: ({ toggle }) => e => toggle(false),
+    toggle: ({ toggle }) => e => toggle(current => !current)
+  })
+);
 
-const neverRender = BaseComponent => props =>
-  class extends Component {
-    shouldComponentUpdate() {
-      return false;
-    }
-    render() {
-      return <BaseComponent {...props} />;
-    }
+function Redux() {
+  return {
+    connect: () => BaseComponent => props => (
+      <BaseComponent
+        {...props}
+        dispatch={({ type }) => console.log(type + " dispatched")}
+      />
+    )
   };
+}
 
-const User2 = neverRender(User);
+const { connect } = Redux();
+
+const StatusList = () => (
+  <div>
+    <div>pending</div>
+    <div>inactive</div>
+    <div>active</div>
+  </div>
+);
+
+const Status = withToggle(({ status, toggledOn, toggle }) => (
+  <span onClick={toggle}>
+    {status}
+    {toggledOn && <StatusList />}
+  </span>
+));
+
+const Tooltip = withToggle(({ text, children, toggledOn, show, hide }) => (
+  <div>
+    {toggledOn && (
+      <div
+        className={"Tooltip"}
+        style={{ background: "aliceblue", color: "gray" }}
+      >
+        {text}
+      </div>
+    )}
+    <div
+      style={{ with: "30rem", height: "2rem" }}
+      onMouseEnter={show || (() => console.log("enter"))}
+      onMouseLeave={hide}
+    >
+      {children}
+    </div>
+  </div>
+));
+
+const enhancer = compose(
+  setDisplayName("User"),
+  setPropTypes({
+    name: PropTypes.string.isRequired,
+    status: PropTypes.string
+  }),
+  connect(),
+  withProps(({ ...props }) => console.log(props) || props)
+);
+
+const User = enhancer(({ name, status, dispatch }) => (
+  <div className={"User"} onClick={() => dispatch({ type: "USER_SELECTED" })}>
+    <Tooltip text={"Cool, Dude!"}>
+      <div>
+        {name} : <Status status={status} />
+      </div>
+    </Tooltip>
+  </div>
+));
+
 const App = () => (
   <div>
-    <User name={"Tim"} />
-    <User2 name={"Weak Ross"} />
+    <User name={"Tim"} status={"active"} />
   </div>
 );
 
