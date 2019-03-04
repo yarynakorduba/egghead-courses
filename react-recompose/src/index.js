7004b1972cf26269fdcd9e380c4bc8d107865f04
yarynakorduba <korduba_y@ucu.edu.ua>
2019-03-04 21:17:37 +0200

    flattened a prop with recompose

--- react-recompose/src/index.js
+++ react-recompose/src/index.js
@@ -2,51 +2,36 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import * as serviceWorker from "./serviceWorker";
-import { mapProps } from "recompose";
+import { compose, flattenProp, withProps } from "recompose";
 
-const users = [
-  {
-    name: "TimActive",
-    status: "active"
-  },
-  {
-    name: "TimPending",
-    status: "pending"
-  },
-  { name: "TimInactive", status: "inactive" },
-  { name: "Tim2Active", status: "active" }
-];
+function ReactRedux() {
+  const state = {
+    user: { name: "Tim", status: "active" }
+  };
 
-const User = ({ name, status }) => (
+  return {
+    connect: map => withProps(map(state))
+  };
+}
+
+const { connect } = ReactRedux();
+
+const mapStateToProps = state => ({ user: state.user });
+
+const enhance = compose(
+  connect(mapStateToProps),
+  flattenProp("user")
+);
+
+const User = enhance(({ name, status }) => (
   <div className={"User"}>
-    {name}
-    {" - " + status}
+    {name} - {status}
   </div>
-);
-
-const UserList = ({ users, status }) => (
-  <div>
-    <h3>{status} users</h3>
-    {users &&
-      users.map((user, ind) => <User key={ind} {...user} status={status} />)}
-  </div>
-);
-
-const filterByStatus = status =>
-  mapProps(({ users }) => ({
-    status,
-    users: users.filter(u => u.status === status)
-  }));
-
-const ActiveUsers = filterByStatus("active")(UserList);
-const InactiveUsers = filterByStatus("inactive")(UserList);
-const PendingUsers = filterByStatus("pending")(UserList);
+));
 
 const App = () => (
   <div>
-    <ActiveUsers users={users} />
-    <InactiveUsers users={users} />
-    <PendingUsers users={users} />
+    <User />
   </div>
 );
 
