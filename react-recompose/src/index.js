44aa4ecc474cf4704c85c24e51073fc4ddf0e21d
yarynakorduba <korduba_y@ucu.edu.ua>
2019-03-04 22:42:37 +0200

    added interface for Links

--- react-recompose/src/index.js
+++ react-recompose/src/index.js
@@ -4,89 +4,33 @@ import "./index.css";
 import * as serviceWorker from "./serviceWorker";
 import {
   branch,
+  componentFromProp,
   compose,
   lifecycle,
   renderComponent,
-  renderNothing
+  renderNothing,
+  withProps
 } from "recompose";
 
-const userIsNotActive = ({ status }) =>
-  console.log(status) || status !== "active";
-const hideIfNotActive = branch(userIsNotActive, renderNothing);
-
-const users = [
-  { name: "Tim", status: "active" },
-  { name: "Bob", status: "active" },
-  { name: "Joe", status: "inactive" },
-  { name: "Jim", status: "pending" }
-];
-const featured = users[3];
-
-function fetchData() {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {}, 1500);
-  });
-}
-
-const withUserData = lifecycle({
-  componentDidMount() {
-    fetchData().then(
-      users => this.setState({ users }),
-      error => this.setState({ error })
-    );
-  }
-});
-
-const errorMsg = {
-  UNAUTHENTICATED: "NOT AUTHENTICATED",
-  UNAUTORIZED: "NOT AUTHORIZED"
-};
-
-const AuthError = ({ error }) => <div>{errorMsg[error.statusCode]}</div>;
-const UsersEmptyState = () => "No users";
-
-const hasErrorCode = ({ error }) => error && error.statusCode;
-const noUsers = ({ users }) => users && !users.length;
-
-const nonOptimalStates = states =>
-  compose(
-    ...states.map(state => branch(state.when, renderComponent(state.render)))
-  );
-const enhancer = compose(
-  withUserData,
-  nonOptimalStates([
-    {
-      when: hasErrorCode,
-      render: AuthError
-    },
-    { when: noUsers, render: UsersEmptyState }
-  ])
-);
-
-const FeaturedUser = hideIfNotActive(({ status, name }) => (
-  <div>
-    Today`s featured user: <User status={status} name={name} />
-  </div>
-));
-
-const UserList = enhancer(() => (
-  <div>
-    {users && users.map((user, id) => <User key={id} {...user} />)}
-    {users && <FeaturedUser status={featured.status} name={featured.name} />}
-  </div>
-));
-
-const User = ({ name, status }) => (
-  <div>
-    <div className={"User"}>
-      {name} - {status}
-    </div>
-  </div>
-);
+const Link = compose(
+  withProps(({ type = "a", to = "#" }) =>
+    type === "a"
+      ? { type, href: to }
+      : {
+          type,
+          onClick(e) {
+            window.location = to;
+          }
+        }
+  )
+)(componentFromProp("type"));
 
 const App = () => (
   <div>
-    <UserList />
+    <Link to={"#/page1"}>Anchor Link</Link>
+    <Link to={"#/page2"} type={"button"}>
+      Button Link
+    </Link>
   </div>
 );
 
